8143856ef9179f0df8828cab41055f4cc0ffe254
José Tomás Navarro Carrión <jt.navarrocarrion@gmail.com>
2016-02-10 18:40:22 +0100

    Moved index creation statements to relational setup

--- sql/functions/setup_relational/relational_setup.sql
+++ sql/functions/setup_relational/relational_setup.sql
@@ -58,6 +58,33 @@ ALTER TABLE siose.siose_values_1 RENAME TO siose_values;
 ALTER TABLE siose.siose_values ADD COLUMN id serial;
 ALTER TABLE siose.siose_values ADD PRIMARY KEY (id);
 
+--Index on polygons
+CREATE INDEX siose_polygons_id_polygon_idx
+  ON siose.siose_polygons
+  USING btree
+  (id_polygon COLLATE pg_catalog."default");
+
+--Index on values
+CREATE INDEX siose_values_inter_id_idx_nulls_low
+  ON siose.siose_values
+  USING btree
+  (inter_id NULLS FIRST);
+
+CREATE INDEX siose_values_id_polygon_idx
+  ON siose.siose_values
+  USING btree
+  (id_polygon COLLATE pg_catalog."default");
+
+CREATE INDEX siose_values_id_cover_idx
+  ON siose.siose_values
+  USING btree
+  (id_cover);
+
+--Index array columns
+CREATE INDEX siose_values_id_parents_idx on siose.siose_values USING GIN (id_parents);
+CREATE INDEX siose_values_inter_parents_idx on siose.siose_values USING GIN (inter_parents);
+CREATE INDEX siose_values_attributes_idx on siose.siose_values USING GIN (attributes);
+
 --Create log table
 CREATE TABLE siose.query_plans
 (
