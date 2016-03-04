80448968ae0c56ca81adb54362b993a822de894d
benizar <benizar@gmail.com>
2016-03-04 10:40:40 +0100

    single backup file

--- data/create_table_spain_boundary_100k.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE siose.spain_boundary_100k
-(
-  id_0 serial NOT NULL,
-  geom geometry(MultiPolygon,4326),
-  tipo_0101 character varying(2),
-  etiqueta character varying(254),
-  CONSTRAINT spain_boundary_100k_pkey PRIMARY KEY (id_0)
-)
