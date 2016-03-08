aa89ba098cf9d3a5aeaa0b986960c696487e1c34
benizar <benizar@gmail.com>
2016-03-08 08:21:56 +0100

    Schemas are created from the extension

---
+++ sql/functions/schemas.sql
@@ -0,0 +1,6 @@
+
+--TODO:test if these schemas are created in time
+CREATE SCHEMA IF NOT EXISTS siose; -- Relational
+CREATE SCHEMA IF NOT EXISTS sioseb; --JSONB
+CREATE SCHEMA IF NOT EXISTS grids;
+CREATE SCHEMA IF NOT EXISTS reports;
